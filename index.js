d06afc830f926b1a71031ed0643778580ce30a96
Jordan Johnson <jkj86@hotmail.com>
2017-09-24 10:33:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function addToCart(item) {
 
 function viewCart() {
   if (cart.length === 0){
-    return "Your shopping cart is empty.";
+    return console.log("Your shopping cart is empty.");
   }
   else if(cart.length===1){
     return "In your cart, you have lemons at "+cart[0]+".";
