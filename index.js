f33fdd90cade72a5c12cc8b405bd480fceb2d408
Jordan Johnson <jkj86@hotmail.com>
2017-09-24 12:33:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber == ""){
+  if (cardNumber == "" || cardNumber == null){
     return console.log("Sorry, we don't have a credit card on file for you.");
   }
   else{
